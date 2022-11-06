3b629043897552741520ac95c33ccef11a44e10f
thevibemonster <yuvrajpruthi1@gmail.com>
2022-11-06 14:40:55 -0500

    Fix documentation, add ranking function

--- Rookie Rank/RookieRank.py
+++ Rookie Rank/RookieRank.py
@@ -10,12 +10,12 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 Players_Sentiment_Data = {}
 
 # get_player_data(first, last, username, tweet_amount) takes an athlete's first name, last name,
-#       and twitter username, and returns tweets equivalent to tweet_amount corresponding to the player
+#       and twitter username, and returns tweet object containing tweets equivalent to tweet_amount 
+#       corresponding to the player
 def get_player_data(first, last, username, tweet_amount):
     client = tweepy.Client(bearer_token=config.BEARER_TOKEN)
     full_name = str(first) + ' ' + str(last)
     query = str(full_name)  + ' -is: retweet' + ' OR ' + str(username) + ' -is: retweet'
-    print(query)
     result = client.search_recent_tweets(query=query, max_results=int(tweet_amount), tweet_fields=['text'])
     return result
 
@@ -27,25 +27,27 @@ def init_player(first, last, username):
     result = get_player_data(first, last, username, 100)
 
     #Initializes playerdatabase
-    dataframe = pd.DataFrame( [tweet.text for tweet in result.data], columns=[str(first) + ' Tweets'])
+    dataframe = pd.DataFrame( [tweet.text for tweet in result.data], 
+                            columns=[str(first) + ' Tweets'])
     pd.set_option('max_colwidth', 200)
     pd.options.display.max_rows = 100000
-    display(dataframe)
-    dataframe.to_csv('tweet_data' + str(first) + '.xls')
+    sentiment_score = [] #Will be added to df as new column
 
-    #Iterates through db to conduct sentiment analysis, storing results in sentiment variable
+    #Iterates through db to conduct sentiment analysis
     sentiment = 0
     SIA = SentimentIntensityAnalyzer()
     for ind in dataframe.index:
         assessed_tweet = SIA.polarity_scores(dataframe[str(first) + ' Tweets'][ind])['compound']
+        sentiment_score.append(assessed_tweet)
         sentiment += assessed_tweet
-    sentiment = (sentiment / 100) #average out coefficient
-    print ('=======================')
-    print(sentiment)
+    sentiment = (sentiment / 100)
 
-    #Appends player's sentiment data to dictionary
+    #Adds sentiment column to df, then exports as CSV
+    dataframe['Sentiment Score'] = sentiment_score
+    dataframe.to_csv('tweet_data' + str(first) + '.csv')
+
+    #Adds player's sentiment data to dictionary
     Players_Sentiment_Data[str(first)] = [sentiment, 100]
-    print(Players_Sentiment_Data)
     return dataframe
 
 # add_data(first, last, dataframe) takes an athlete's first name, last name,
@@ -55,22 +57,25 @@ def add_data(first, last, username, dataframe):
     #Retrieves 100 tweets to add to db
     result = get_player_data(first, last, username, 100)
     
-    #Creates temp dataframe to iterate through for sentiment analysis
-    dataframe_temp = pd.DataFrame( [tweet.text for tweet in result.data], columns=[str(first) + ' Tweets'])
+    #Creates temp dataframe to concat to total data
+    dataframe_temp = pd.DataFrame([tweet.text for tweet in result.data], 
+                                columns=[str(first) + ' Tweets'])
     pd.set_option('max_colwidth', 200)
     pd.options.display.max_rows = 100
-    display(dataframe_temp)
-    dataframe_temp.to_csv('concat_test.xls', index=False)
+    sentiment_score = [] # Will be added to df as new column
 
-    #Iterates through db to conduct sentiment analysis, storing results in sentiment variable
+    #Conducts sentiment analysis on new tweets
     sentiment = 0
     SIA = SentimentIntensityAnalyzer()
     for ind in dataframe_temp.index:
         assessed_tweet = SIA.polarity_scores(dataframe_temp[str(first) + ' Tweets'][ind])['compound']
+        sentiment_score.append(assessed_tweet)
         sentiment += assessed_tweet
-    print ('=======================')
+    
+    #Adds sentiment column to temp df
+    dataframe_temp['Sentiment Score'] = sentiment_score
 
-    #Updates dictionary's sentiment coefficient
+    #Updates dictionary with new opinion coefficient
     sentiment_old = Players_Sentiment_Data[str(first)][0]
     count_oldtweets = Players_Sentiment_Data[str(first)][1]
     new_count = count_oldtweets + 100
@@ -78,16 +83,34 @@ def add_data(first, last, username, dataframe):
     Players_Sentiment_Data[str(first)][0] = new_sentiment
     Players_Sentiment_Data[str(first)][1] = new_count
 
-    #Concatenates temp dataframe into player's existing dataframe
+    #Concatenates temp dataframe into existing data
     dataframe = pd.concat([dataframe, dataframe_temp], ignore_index=True)
-    dataframe.to_csv('tweet_data' + str(first) + '.xls')
+    dataframe.to_csv('tweet_data' + str(first) + '.csv')
 
     return dataframe
+
+# show_rankings() iterates through Player_Sentiment_Data dictionary
+#       and prints players rankings in order
+def show_rankings():
+    i = 1
+    for k, v in sorted(Players_Sentiment_Data.items(), key = lambda x:x[1][0], reverse = True):
+        print("#" + str(i) + ") " + k, '-->', v[0])
+        i += 1
 
 
 
 
 df_steph = init_player('Steph', 'Curry', '@StephenCurry30')
 df_steph = add_data('Steph', 'Curry', '@StephenCurry30', df_steph)
+
+df_lebron = init_player('Lebron', 'James', '@KingJames')
+df_lebron = add_data('Lebron', 'James', '@KingJames')
+
+df_kevin = init_player('Kevin', 'Durant', '@KDTrey5')
+df_kevin = add_data('Kevin', 'Durant', '@KDTrey5')
+
+show_rankings()
+
+
 
 
